e1a651c8f548195a705159fe14c6606914642ef7
kush-x7 <kushagratripathi7@gmail.com>
2021-01-25 13:28:21 +0530

    remove indentation

--- 1_Variables/variables.py
+++ 1_Variables/variables.py
@@ -1,48 +1,61 @@
 # How to initilize a variable
 
-    s = 3600                  # integer
-    seconds = 3600            # integer
-    seconds_per_hour = 3600   # integer
+s = 3600                  # integer
+seconds = 3600            # integer
+seconds_per_hour = 3600   # integer
 
-    name = "kush"             #string
+name = "kush"             #string
 
-    choice =True              #boolean
+choice =True              #boolean
 
+--------------------------------------------------------------------
 
 # How to print a variable
 
-    print(s)                # output-> 3600
-    print(seconds)          # output-> 3600
-    print(seconds_per_hour) # output-> 3600
+print(s)                # output-> 3600
+print(seconds)          # output-> 3600
+print(seconds_per_hour) # output-> 3600
+
+--------------------------------------------------------------------
 
 # Things to remember
-    # 1-> correct way for naming variable
-            KUSH
-            kush
-            _kush
-            kush_age
 
-    # 2-> Wrong way for naming variable
-        9lives
-        99_balloons
-        2beOrNot2BeInadditiontoEng
+# 1-> correct way for naming variable
 
-    # 3-> Case sensetive
-        seconds =3600
-        print(SECONDS)   #it will show error
-                         # name 'SECONDS' is not defined
+KUSH
+kush
+_kush
+kush_age
 
-    # 4-> Never initilize an empty variable
-        hours  #Wrong  because you have to give some value to store
+# 2-> Wrong way for naming variable
 
-        hours = 0  #correct
-        hr = ""    #correct
+9lives
+99_balloons
+2beOrNot2BeInadditiontoEng
 
-    # 5-> You can change value later on
-        num =65
-        num =23
+# 3-> Case sensetive
+
+seconds =3600
+print(SECONDS)   #it will show error
+# name 'SECONDS' is not defined
+
+# 4-> Never initilize an empty variable
+
+hours  #Wrong  because you have to give some value to store
+
+hours = 0  #correct
+hr = ""    #correct
+
+# 5-> You can change value later on
+
+num =65
+num =23
+
+--------------------------------------------------------------------
 
 # We can save a variable in a variable
 
-    final = s + s
-    print(final)             #->7200
+final = s + s
+print(final)             #->7200
+
+--------------------------------------------------------------------
--- 2_comments/comments.py
+++ 2_comments/comments.py
@@ -1,11 +1,12 @@
 ->We have 2 types of comment
 
-    1- > Block comment
-     # This is a block comment
+1- > Block comment
+# This is a block comment
 
-    2-> Inline comment
-     print(greeting)  # This is an inline comment.
+2-> Inline comment
+print(greeting)  # This is an inline comment.
 
+--------------------------------------------------------------------
 ->Sample
 
 # This is my first program.
