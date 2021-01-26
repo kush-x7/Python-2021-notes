c9c63d4d008cc40e190082190c929ac78de1edb6
kush-x7 <kushagratripathi7@gmail.com>
2021-01-26 21:31:13 +0530

    commit "name generator problem"

---
+++ 3_important function/important.py
@@ -0,0 +1,44 @@
+#total function i have learned
+
+print()    ->for printing
+
+input()    ->for taking INPUTTING
+
+int()      ->for converting other data types like "Sting" into an integer
+
+float()     ->for converting other data types like unteger into an float
+
+str()       -> 9 into "9"
+
+type()     -> for checking data type
+
+
+len()      ->gives length of the "String"
+
+title()    -> It makes the the first char of string upper case -> string ="hello kush" ->string.title() - >Hello Kush
+
+.lower()   ->lower case "String"
+
+.count()   ->use to count particular char in a string  -> name.count("A")  ->3
+
+round()    ->for rounding of values    round(x ,2) round of ang give 2 decimal places
+
+
+
+
+
+
+
+sum()      ->sum of lists
+
+.split()   -> it seperate 2 string with " ",  ->kush sam -> "kush", "sam"
+
+.append()  -> inserting value in a list  ->alphabet.append("v")
+
+.extend()  -> inserting a list in a list ->alphabet.extend(['s', 'l'])
+
+.remove()  -> for removing element from the list -> .remove(11)
+
+open("filename.txt")  ->for opening text file
+
+.read()    -> reading the txt file
---
+++ programs/name_generator.py
@@ -0,0 +1,14 @@
+#1. Create a greeting for your program.
+print("Welcome to the name generator.")
+
+#2. Ask the user to input his/her first name
+first_name = input("Enter your first name \n-> ")
+
+#3. Ask the user to input his/her last  name
+last_name = input("Enter your last name \n-> ")
+
+#4. Combine both the names
+name = first_name + " " + last_name
+
+#5. print the final output
+print("Your full name is " +  name)
